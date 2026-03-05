cd69301632396831622b7c58781105f913be0c07
maddurimanikantareddy <maddurimanikantareddy18@gmail.com>
2026-03-05 16:46:04 +0530

    secondcommit

--- app/src/main/java/com/greenguide/greenguide/MainActivity.kt
+++ app/src/main/java/com/greenguide/greenguide/MainActivity.kt
@@ -1,47 +1,58 @@
 package com.greenguide.greenguide
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.greenguide.greenguide.ui.theme.GreenguideTheme
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            GreenguideTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+        setContentView(R.layout.activity_main)
+
+        locationText = findViewById(R.id.locationText)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        getLocation()
+    }
+
+    private fun getLocation() {
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                1
+            )
+            return
+        }
+
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+
+                if (location != null) {
+
+                    val latitude = location.latitude
+                    val longitude = location.longitude
+
+                    locationText.text =
+                        "Latitude: $latitude\nLongitude: $longitude"
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GreenguideTheme {
-        Greeting("Android")
     }
 }
